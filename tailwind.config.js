a89552defde6476e33e39ef8e4c01f6860f11e4b
Zuoers <67604119+Zuoers@users.noreply.github.com>
2021-07-03 10:34:31 +0100

    Transfer-Files/Logos

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        sweetBlack: "#17171A",
+        shinyGray: "#1D1E23",
+        beautyPurple: "#9F72DA",
+        blurple: "#7289DA"
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
